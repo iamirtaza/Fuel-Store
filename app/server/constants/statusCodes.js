613b3fe469eae1137568d6fd0da1d7a161ede5f0
Syed Irtaza Mubashar <syedirtazamubashar@gmail.com>
2020-01-14 13:56:51 +0500

    fuel app front end

--- app/server/constants/statusCodes.js
+++
@@ -1,3 +0,0 @@
-module.exports.statusCodes = {
-    VALIDATION_FAILED: 412
-};
--- app/server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const db = require("../helpers/db");
-const { getFailureResponse, getSuccessResponse } = require("../helpers/response");
-
-router.use("/users", require("./user.routes"));
- 
-
-module.exports = router;
